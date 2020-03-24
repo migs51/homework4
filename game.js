22088306a6260d4cc4b29e86f046b27a19013ffb
Migs <mmejares@hotmail.com>
2020-03-24 15:05:18 -0500

    added questions array

--- game.js
+++ game.js
@@ -1,25 +1,39 @@
-//click event to start Quiz and timer function
-$("#startButton").click(function(){
-    timer();
+$(document).ready(function() {
 
-})
+    //global variables
+    var questions = $("#questions");
+    var answers = $("#answers");
 
-//timer variables
-var ONE_SECOND = 1000;
 
-var secondsLeft = 60;
+    //click event to start Quiz, timer function and 1st question/answers
+    $("#startButton").click(function(){
+        timer();
 
-//timer function
-function timer(){
-    var timerID = setInterval(function(){
-    console.log("one second has passed")
-    secondsLeft--;
-    console.log(secondsLeft);
-    $("#timer").text(secondsLeft);
-    if (secondsLeft <= 0){
-        clearInterval(timerID);
+    })
+
+    //append questions and answers
+
+    //append "Correct" or "Wrong" Alert
+
+
+    //click event when answering a question
+
+    //timer variables
+    var ONE_SECOND = 1000;
+
+    var secondsLeft = 60;
+
+    //timer function
+    function timer(){
+        var timerID = setInterval(function(){
+        console.log("one second has passed")
+        secondsLeft--;
+        console.log(secondsLeft);
+        $("#timer").text(secondsLeft);
+        if (secondsLeft <= 0){
+            clearInterval(timerID);
+        }
+    }, 1000);
     }
-}, 1000);
+
 }
-
-
---
+++ questions.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+        title: "There are versions of 'The Office' in a few different countries. What is the French version called?",
+        choices: ["Le Bureau", "Le Office", "The Office","The Office: French Edition"], 
+        answer:"Le Bureau"
+    },
+    {
+        title:"What sort of shoes does Clark suggest Andy should buy for Erin to wear to the newscaster audition he will be filming for her in his apartment?",
+        choices: ["sneakers", "high heels", "open-toed heels", "flip-flops"],
+        answer: "open-toed heels"
+    }
+]
+
