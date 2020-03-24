aa955c5defa8dfa838abfe46d99641de40f0d9ae
Migs <mmejares@hotmail.com>
2020-03-23 23:42:04 -0500

    created click event for start button and timer function

--- game.js
+++ game.js
@@ -1,18 +1,25 @@
-//click event to start Quiz and timer
+//click event to start Quiz and timer function
 $("#startButton").click(function(){
-    alert("It Works!");
+    timer();
 
 })
 
-//timer
+//timer variables
 var ONE_SECOND = 1000;
-var FIFTEEN_SECONDS = 15 * ONE_SECOND;
 
 var secondsLeft = 60;
 
-var timerID = setInterval(function(){
-    //this happens every second
+//timer function
+function timer(){
+    var timerID = setInterval(function(){
     console.log("one second has passed")
+    secondsLeft--;
+    console.log(secondsLeft);
+    $("#timer").text(secondsLeft);
+    if (secondsLeft <= 0){
+        clearInterval(timerID);
+    }
 }, 1000);
+}
 
-//when does this end, when the timer is 0 seconds
+
